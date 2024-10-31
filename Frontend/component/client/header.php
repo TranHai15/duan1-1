6ace9adb48bedd8233469420874b44b59e18765d
TranHai <hairobet15092005@gmail.com>
2024-10-31 10:42:19 +0700

    .home

--- Frontend/component/client/header.php
+++ Frontend/component/client/header.php
@@ -5,13 +5,115 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
-    <link rel="stylesheet" href="Frontend/Css/reset.css">
-    <link rel="stylesheet" href="Frontend/Css/grid.css">
-    <link rel="stylesheet" href="Frontend/Css/hf.css">
+    <link rel="stylesheet" href="Frontend/Css/reset.css?ver=6">
+    <link rel="stylesheet" href="Frontend/Css/grid.css?ver=3">
+    <link rel="stylesheet" href="Frontend/Css/hf.css?ver=34">
 </head>
 
 <body>
-    <h1>helo</h1>
+    <div class="header__kc">
+        <header>
+            <div class="header grid wide">
+                <nav class="header__left row align-items-center">
+                    <a href="#">
+                        <div class="header__logo">
+                            <img src="Frontend/public/svg/logo.svg" alt="Yody">
+                        </div>
+                    </a>
+                    <div class="menu">
+                        <span class="menu-item"><a href="#" class="menu-item-a ">SALE</a>
+                            <div class="overlay"></div>
+                            <div class="menu__list--cha">
+                                <div class="menu__list grid wide row">
+                                    <div class="item__list ">
+                                        <div class="child__category"> <a href="#">Ao</a></div>
+                                        <div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                        </div>
+                                    </div>
+                                    <div class="item__list ">
+                                        <div class="child__category"> <a href="#">Ao</a></div>
+                                        <div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+
+                                        </div>
+                                    </div>
+                                    <div class="item__list ">
+                                        <div class="child__category"> <a href="#">Ao</a></div>
+                                        <div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                        </div>
+                                    </div>
+                                    <div class="item__list ">
+                                        <div class="child__category"> <a href="#">Ao</a></div>
+                                        <div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                        </div>
+                                    </div>
+                                    <div class="item__list ">
+                                        <div class="child__category"> <a href="#">Ao</a></div>
+                                        <div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                            <div class="commont__category"><a href="#">Ao Polo</a></div>
+                                        </div>
+                                    </div>
+                                    <div class="item__list--img">
+                                        <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/media/ecom/2023-06-12-08-48-19_a5b00606-d7c0-4ba0-9611-33867680f45b.webp"
+                                            alt="">
+                                    </div>
+                                </div>
+                            </div>
+
+                        </span>
+
+                        <span class="menu-item"><a href="#" class="menu-item-a ">Mới về</a></span>
+                        <span class="menu-item"><a href="#" class="menu-item-a ">Nam </a></span>
+                        <span class="menu-item"><a href="#" class="menu-item-a ">Nữ</a></span>
+                        <span class="menu-item"><a href="#" class="menu-item-a ">Trẻ em</a></span>
+                        <span class="menu-item"><a href="#" class="menu-item-a ">Bộ sưu tập</a></span>
+                    </div>
+                </nav>
+
+
+                <span class="header__right row align-items-center">
+                    <span class="header__search">
+                        <button><img src="Frontend/public/svg/search.svg" alt="Yody-btn"></button>
+                        <input type="text" id="header__search--input " class="header__search--input"
+                            placeholder="Tìm kiếm ">
+                    </span>
+                    <span class="header__cart">
+                        <img loading="lazy" src="Frontend/public/svg/cart.svg" alt="Yody-cart">
+                    </span>
+                    <span class="header__user"> <img loading="lazy" src="Frontend/public/svg/account.svg"
+                            alt="Yody-user"></span>
+                </span>
+            </div>
+        </header>
+    </div>
+
 </body>
 
 </html>
--- Frontend/component/client/views/Home.php
+++ Frontend/component/client/views/Home.php
@@ -2,13 +2,340 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
 </head>
+<link rel="stylesheet" href="Frontend/Css/reset.css?ver=2">
+<link rel="stylesheet" href="Frontend/Css/grid.css?ver=1">
+<link rel="stylesheet" href="Frontend/Css/home.css?ver=3" />
 
 <body>
-    <h1>Home</h1>
+    <section class="">
+        <header></header>
+        <section class="banner1 grid wide">
+            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/fit-in/filters:format(webp)/products/m2icqji8qcsbiegy6ildp-hlw-1800x833.png"
+                alt="" />
+        </section>
+        <main>
+            <div class="grid wide ">
+                <h2 class="titile1">Sản phẩm ưa chuộng</h2>
+                <section class="nav ">
+                    <ul class="menu">
+                        <li class="menu-item active"><a href="#">Áo Gió</a></li>
+                        <li class="menu-item"><a href="#">Jeans Flex</a></li>
+                        <li class="menu-item"><a href="#">Áo Polo</a></li>
+                        <li class="menu-item"><a href="#">Quần Âu</a></li>
+                        <li class="menu-item"><a href="#">Sơ Mi</a></li>
+                    </ul>
+                </section>
+                <section class="products row  justify-content-between grid wide">
+                    <article class="product l-3 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-khoac-the-thao-nu-SKN7007-GNH%20(10).JPG"
+                                alt="" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation  "></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+                    <article class="product l-3 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-khoac-the-thao-nu-SKN7007-GNH%20(10).JPG"
+                                alt="" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+                    <article class="product l-3 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-khoac-the-thao-nu-SKN7007-GNH%20(10).JPG"
+                                alt="" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+                    <article class="product l-3 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-khoac-the-thao-nu-SKN7007-GNH%20(10).JPG"
+                                alt="" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+
+                </section>
+                <button class="btn-center">Xem thêm</button>
+            </div>
+            <section class="banner2 grid wide">
+                <img src="https://m.yodycdn.com/fit-in/filters:format(webp)//products/Collection%20List%20Banner_Website_1800x600.jpg"
+                    alt="" />
+            </section>
+            <div class="grid wide">
+                <h2 style="color: #4B6280;">Gợi ý sản phẩm</h2>
+
+
+                <section class="products row  justify-content-between">
+                    <article class="product product-5 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-giu-nhiet-nu-yody-ATN7007-DEN%20(13).jpg"
+                                alt="Áo Thu Đông Trẻ Em Giữ Nhiệt Cổ Lọ" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+                    <article class="product product-5 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-giu-nhiet-nu-yody-ATN7007-DEN%20(13).jpg"
+                                alt="Áo Thu Đông Trẻ Em Giữ Nhiệt Cổ Lọ" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+                    <article class="product product-5 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-giu-nhiet-nu-yody-ATN7007-DEN%20(13).jpg"
+                                alt="Áo Thu Đông Trẻ Em Giữ Nhiệt Cổ Lọ" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+                    <article class="product product-5 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-giu-nhiet-nu-yody-ATN7007-DEN%20(13).jpg"
+                                alt="Áo Thu Đông Trẻ Em Giữ Nhiệt Cổ Lọ" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+                    <article class="product product-5 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-giu-nhiet-nu-yody-ATN7007-DEN%20(13).jpg"
+                                alt="Áo Thu Đông Trẻ Em Giữ Nhiệt Cổ Lọ" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+
+
+                </section>
+                <section class="products row  justify-content-between">
+                    <article class="product product-5 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-giu-nhiet-nu-yody-ATN7007-DEN%20(13).jpg"
+                                alt="Áo Thu Đông Trẻ Em Giữ Nhiệt Cổ Lọ" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+                    <article class="product product-5 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-giu-nhiet-nu-yody-ATN7007-DEN%20(13).jpg"
+                                alt="Áo Thu Đông Trẻ Em Giữ Nhiệt Cổ Lọ" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+                    <article class="product product-5 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-giu-nhiet-nu-yody-ATN7007-DEN%20(13).jpg"
+                                alt="Áo Thu Đông Trẻ Em Giữ Nhiệt Cổ Lọ" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+                    <article class="product product-5 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-giu-nhiet-nu-yody-ATN7007-DEN%20(13).jpg"
+                                alt="Áo Thu Đông Trẻ Em Giữ Nhiệt Cổ Lọ" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+                    <article class="product product-5 m-4 c-12">
+                        <div class="product-image">
+                            <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/ao-giu-nhiet-nu-yody-ATN7007-DEN%20(13).jpg"
+                                alt="Áo Thu Đông Trẻ Em Giữ Nhiệt Cổ Lọ" />
+                        </div>
+                        <p class="product-name">Áo Khoác Thể Thao Nữ Siêu Nhẹ Chống Tia UV</p>
+                        <div class="product-price">
+                            <p class="price-new">599.000 đ</p>
+                            <span class="price-old"></span>
+                        </div>
+                        <div class="product-variation">
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                            <span class="item-variation"></span>
+                        </div>
+                    </article>
+
+
+                </section>
+
+                <button class="btn-center">Xem thêm</button>
+
+            </div>
+            <section class="events row grid wide">
+                <a href="#" class=""><img
+                        src="https://m.yodycdn.com/fit-in/filters:format(webp)//products/YODY%20x%20Wintel_893x598.jpg"
+                        alt="" srcset="" />
+                </a>
+                <a href="#"><img src="https://m.yodycdn.com/fit-in/filters:format(webp)//products/YODY_XANH%20SM_893x598.jpg
+          " srcset="" /></a>
+                <a href="#"><img src="https://m.yodycdn.com/fit-in/filters:format(webp)//products/Frame%2032%201.jpg"
+                        alt="" srcset="" /></a>
+                <a href="#">
+                    <img src="https://m.yodycdn.com/fit-in/filters:format(webp)//products/Frame%2031%201.jpg" alt=""
+                        srcset="" /></a>
+            </section>
+            <section class="title">
+                <h1>#Yody tự hào thương hiệu Việt</h1>
+            </section>
+            <section class="posts  grid wide">
+                <div class="post">
+                    <a href="#">
+                        <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/media/articles/yody-top-10-thuong-hieu-thoi-trang.jpg"
+                            alt="" />
+                        <p class="post-title">YODY lọt Top 10 thương hiệu thời trang lớn nhất tại Đông</p>
+                        <p class="post-detail">
+                            YODY được website Campaign Asia-Pacific vinh danh là Thương Hiệu
+                            Thời Trang thuộc Top 7 Đông Nam Á & Top 10 Việt Nam.
+                        </p>
+                    </a>
+                </div>
+                <div class="post">
+                    <a href="#">
+                        <img src="https://m.yodycdn.com/fit-in/filters:format(webp)/products/media/articles/280.jpeg"
+                            alt="" />
+                        <p class="post-title">Cán Mốc 280 Cửa Hàng - YODY Chinh Phục Mọi Miền</p>
+                        <p class="post-detail">
+                            Thương hiệu thời trang Việt Nam YODY vừa ghi dấu ấn mới trong hành
+                            trình phát triển của mình khi chính thức cán mốc 280 cửa hàng trên
+                            toàn quốc.
+                        </p>
+                    </a>
+                </div>
+            </section>
+        </main>
+        <footer></footer>
+    </section>
 </body>
 
 </html>
